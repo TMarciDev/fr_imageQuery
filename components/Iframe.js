2434b1451462fbf4da07994033a872177a5ad0eb
Tóth Márton <ejjb7g@inf.elte.hu>
2022-03-28 02:22:27 +0200

    gif searc api added with search button

---
+++ components/Iframe.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const Iframe = () => {
+  return (
+    <>Iframe</>
+  )
+}
+
+export default Iframe
---
+++ pages/ddg_image_utils/constants.js
@@ -0,0 +1,28 @@
+module.exports = {
+	url: 'https://duckduckgo.com/',
+	headers: {
+		'Access-Control-Allow-Origin': '*',
+		'Access-Control-Allow-Methods': 'DELETE, POST, GET, OPTIONS',
+		'Access-Control-Allow-Headers':
+			'Content-Type, Authorization, X-Requested-With',
+		//dnt: '1',
+		//'accept-encoding': 'gzip, deflate, sdch',
+		'x-requested-with': 'XMLHttpRequest',
+		'accept-language': 'en-GB,en-US;q=0.8,en;q=0.6,ms;q=0.4',
+		//'user-agent':
+		//	'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/55.0.2883.87 Safari/537.36',
+		accept: 'application/json, text/javascript, */*; q=0.01',
+		//referer: 'https://duckduckgo.com/',
+		authority: 'duckduckgo.com',
+	},
+	max_iter: 2,
+	max_retries: 2,
+	params_template: {
+		l: 'wt-wt',
+		o: 'json',
+		q: null,
+		vqd: null,
+		f: ',,,',
+		p: null,
+	},
+};
